7df520be9dfcea22f59ba14ae87fac1004912ef4
ContentJeka <jekchannel228@gmail.com>
2022-05-21 22:26:52 +0300

    Update fxmanifest.lua

--- fivem/fxmanifest.lua
+++ fivem/fxmanifest.lua
@@ -5,6 +5,4 @@ author 'An awesome dude'
 description 'An awesome, but short, description'
 version '1.0.0'
 
-resource_type 'gametype' { name = 'My awesome game type!' }
-
-client_script 'mymode_client.js'
+client_script {'mymode_client.js'}
